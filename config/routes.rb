08d19002c40dfbdd11eee7c12d4bb024ce38939b
Symion Edwards <symion.edwards@gmail.com>
2021-01-06 11:34:18 +0000

    Added a like and unlike button to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
   devise_for :users, :controllers => {
     registrations: 'registrations'
   }
