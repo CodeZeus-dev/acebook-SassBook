83d4b9cc3e349bc4e91508b01b5077a61c2367b6
Melissa <mystic101@gmail.com>
2021-01-06 14:26:17 +0000

    Added methods to the application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,24 @@ module ApplicationHelper
     def notification_find(notice, type)
         return User.find(notice.notice_id) if type == 'friendRequest'
     end
+
+    def friend_request_sent?(user)
+        current_user.friend_sent.exists?(sent_to_id: user.id, status: false)
+      end
+      
+      def friend_request_received?(user)
+        current_user.friend_request.exists?(sent_by_id: user.id, status: false)
+      end
+      
+      # Checks whether a user has had a friend request sent to them by the current user or 
+      # if the current user has been sent a friend request by the user returning either true or false
+      def possible_friend?(user)
+        request_sent = current_user.friend_sent.exists?(sent_to_id: user.id)
+        request_received = current_user.friend_request.exists?(sent_by_id: user.id)
+        
+        return true if request_sent != request_recieved    
+        return true if request_sent == request_recieved && request_sent == true    
+        return false if request_sent == request_recieved && request_sent == false
+      end
+    
 end
