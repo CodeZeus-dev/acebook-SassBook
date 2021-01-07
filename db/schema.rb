fd7f4a73a95cd86af677469901122740c69530eb
Melissa <mystic101@gmail.com>
2021-01-07 15:14:33 +0000

    Initial commit after upgrading to Rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210106153655) do
+ActiveRecord::Schema.define(version: 2021_01_06_153655) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "sent_to_id", null: false
