9f57049a3778ec5f0e1c1fe932c8ebf4e11c0ff8
CodeZeus-dev <constantine.akritides@gmail.com>
2021-01-07 16:35:24 +0200

    debugging the friendship and like specs (not working)

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
 
+    login_user
+
+    let(:valid_attributes) {
+        { 
+            :postBody => "Post Body",
+            :user_id => 1
+        }   
+    }
+
+    before(:each) do
+        @post_attributes = FactoryBot.attributes_for(:post, :user_id => @user)
+    end
+
+    describe 'POST #create' do
+        it "POSTs a new like to a post" do
+            post :create, params: { user_id: @user.id, post_id: @post_attributes.id }
+            expect { response }.to change(Like, :count).by(1)
+        end 
+    end
 end
